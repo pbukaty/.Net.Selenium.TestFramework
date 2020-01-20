0aa622a86375b3a8a2d6b28435c89387fbc06c2c
Pavel Bukaty <p.bukatyj@intetics.com>
2020-01-20 18:09:22 +0300

    Implement WebDriverUtils

---
+++ src/TestFramework/Utils/WebDriverUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace TestFramework.Utils
+{
+    public class WebDriverUtils
+    {
+        private readonly IWebDriver _driver;
+        public WebDriverUtils(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void TakeScreenshot(string screenshotName)
+        {
+            Screenshot screenshot = ((ITakesScreenshot) _driver).GetScreenshot();
+            screenshot.SaveAsFile($"{AppDomain.CurrentDomain.BaseDirectory}TestResults\\{screenshotName}.png");
+        }
+    }
+}
