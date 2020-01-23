52ea48f71af14415d403a15820ec8789735d9760
Pavel Bukaty <p.bukatyj@intetics.com>
2020-01-23 19:04:24 +0300

    Update GetElement processing

--- src/TestFramework/Factory/WebElementFactory.cs
+++ src/TestFramework/Factory/WebElementFactory.cs
@@ -7,56 +7,69 @@ namespace TestFramework.Factory
 {
     public class WebElementFactory
     {
-        public IWebElement FindWebElement(IWebDriver driver, Properties properties)
+        private readonly IWebDriver _driver;
+        public WebElementFactory(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebElement FindWebElement(Properties properties)
         {
             var locator = properties.Locator;
             var locatorType = properties.LocatorType ?? "XPath";
 
-            switch (locatorType)
+            try
             {
-                case "Id":
-                    return driver.FindElement(By.Id(locator));
-                case "Name":
-                    return driver.FindElement(By.Name(locator));
-                case "XPath":
-                    return driver.FindElement(By.XPath(locator));
-                case "CssSelector":
-                    return driver.FindElement(By.CssSelector(locator));
-                case "ClassName":
-                    return driver.FindElement(By.ClassName(locator));
-                case "LinkText":
-                    return driver.FindElement(By.LinkText(locator));
-                case "PartialLinkText":
-                    return driver.FindElement(By.PartialLinkText(locator));
-                case "TagName":
-                    return driver.FindElement(By.TagName(locator));
-                default:
-                    throw new NotSupportedException($"'{locatorType}' is not supported locator type");
+                switch (locatorType)
+                {
+                    case "Id":
+                        return _driver.FindElement(By.Id(locator));
+                    case "Name":
+                        return _driver.FindElement(By.Name(locator));
+                    case "XPath":
+                        return _driver.FindElement(By.XPath(locator));
+                    case "CssSelector":
+                        return _driver.FindElement(By.CssSelector(locator));
+                    case "ClassName":
+                        return _driver.FindElement(By.ClassName(locator));
+                    case "LinkText":
+                        return _driver.FindElement(By.LinkText(locator));
+                    case "PartialLinkText":
+                        return _driver.FindElement(By.PartialLinkText(locator));
+                    case "TagName":
+                        return _driver.FindElement(By.TagName(locator));
+                    default:
+                        throw new NotSupportedException($"'{locatorType}' is not supported locator type");
+                }
+            }
+            catch
+            {
+                return null;
             }
         }
 
-        public IEnumerable<IWebElement> FindWebElements(IWebDriver driver, Properties properties)
+        public IEnumerable<IWebElement> FindWebElements(Properties properties)
         {
             var locator = properties.Locator;
             var locatorType = string.IsNullOrEmpty(properties.LocatorType) ? "XPath" : properties.LocatorType;
             switch (locatorType)
             {
                 case "Id":
-                    return driver.FindElements(By.Id(locator));
+                    return _driver.FindElements(By.Id(locator));
                 case "Name":
-                    return driver.FindElements(By.Name(locator));
+                    return _driver.FindElements(By.Name(locator));
                 case "XPath":
-                    return driver.FindElements(By.XPath(locator));
+                    return _driver.FindElements(By.XPath(locator));
                 case "CssSelector":
-                    return driver.FindElements(By.CssSelector(locator));
+                    return _driver.FindElements(By.CssSelector(locator));
                 case "ClassName":
-                    return driver.FindElements(By.ClassName(locator));
+                    return _driver.FindElements(By.ClassName(locator));
                 case "LinkText":
-                    return driver.FindElements(By.LinkText(locator));
+                    return _driver.FindElements(By.LinkText(locator));
                 case "PartialLinkText":
-                    return driver.FindElements(By.PartialLinkText(locator));
+                    return _driver.FindElements(By.PartialLinkText(locator));
                 case "TagName":
-                    return driver.FindElements(By.TagName(locator));
+                    return _driver.FindElements(By.TagName(locator));
                 default:
                     throw new NotSupportedException($"'{locatorType}' is not supported locator type.");
             }
--- src/TestFramework/WebPages/WebPage.cs
+++ src/TestFramework/WebPages/WebPage.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using TestFramework.Extensions;
 using TestFramework.Factory;
 using TestFramework.Models;
 using TestFramework.Readers;
@@ -8,19 +11,57 @@ namespace TestFramework.WebPages
 {
     public class WebPage: BasePage
     {
-        public string PageName { get; }
         public Dictionary<string, Properties> WebElementsDictionary { get; private set; }
+        public string PageName { get; }
+
+        private readonly WebElementFactory _webElementFactory;
 
         public WebPage(IWebDriver driver, string pageName) : base(driver)
         {
             var page = JsonDataReader.LoadElements($"{pageName}.json");
             InitWebElementsDictionary(page.Elements);
             PageName = pageName;
+            _webElementFactory = new WebElementFactory(Driver);
         }
 
-        public IWebElement GetWebElement(string elementName)
+        public IWebElement GetWebElement(string elementName, bool shouldExist = true)
         {
-            return new WebElementFactory().FindWebElement(Driver, WebElementsDictionary[elementName]);
+            try
+            {
+                var element = _webElementFactory.FindWebElement(WebElementsDictionary[elementName]);
+                if (shouldExist)
+                {
+                    element.Should().BeFound(elementName, BuildErrorAdditionalInfo(elementName));
+                }
+                else
+                {
+                    element.Should().NotBeFound(elementName, BuildErrorAdditionalInfo(elementName));
+                }
+
+                return element;
+            }
+            catch (NotSupportedException ex)
+            {
+                throw new NotSupportedException($"{ex.Message}. {BuildErrorAdditionalInfo(elementName)}");
+            }
+        }
+
+        public IEnumerable<IWebElement> GetWebElements(string elementName)
+        {
+            return _webElementFactory.FindWebElements(WebElementsDictionary[elementName]);
+        }
+
+        public SelectElement GetDropDown(string elementName)
+        {
+            var element = GetWebElement(elementName);
+            try
+            {
+                return new SelectElement(element);
+            }
+            catch (UnexpectedTagNameException ex)
+            {
+                throw new UnexpectedTagNameException($"{ex.Message}. {BuildErrorAdditionalInfo(elementName)}");
+            }
         }
 
         private void InitWebElementsDictionary(IEnumerable<Element> elements)
@@ -30,6 +71,12 @@ namespace TestFramework.WebPages
             {
                 WebElementsDictionary.Add(element.Name, element.Properties);
             }
+        }
+
+        private string BuildErrorAdditionalInfo(string elementName)
+        {
+            return
+                $"Element info: Page is '{PageName}'; element name: '{elementName}'; element locator: '{WebElementsDictionary[elementName].Locator}'";
         }
     }
 }
